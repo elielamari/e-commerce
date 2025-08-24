22cb60db02c1533032faa203cfdfad5ac834d6ab
elielamari <amarieliel18@gmail.com>
2025-08-24 20:09:24 +0530

    new migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,9 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'usertype',
+        'phone',
+        'password'
     ];
 
     /**
