fcd48e5a8d27206f36909451b2cab6c9817dd9ad
elielamari <amarieliel18@gmail.com>
2025-08-30 19:32:32 +0530

    test

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,7 +33,8 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
+        ]); 
+        
 
         $user = User::create([
             'name' => $request->name,
